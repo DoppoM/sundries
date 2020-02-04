5157219eefc8ff6d825247339ad44cbd3572b10d
Михаил Башметов <gicata.sr@gmail.com>
2020-02-04 14:26:38 +0300

    started working on the rtmp server script.
    [IN PROCESS]  start discribe parametrize gcp instance

---
+++ rtmpServerDeployment.ps1
@@ -0,0 +1,10 @@
+﻿param(
+    $cloudProvider = "gcp",
+    $instanceName,
+    $networkName = "$instanceName-netwark",
+    $subnet = "$instanceName-subnet",
+    $image = "ubuntu-1604-xenial-v20200129",
+    $machineType = "n1-highcpu-8"
+)
+
+gcloud compute instances create $instanceName --network $networkName --subnet $subnet --image $image --machine-type $machineType
